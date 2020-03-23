05844bc60aa68f40be1f212fd30d08a56c9c06d5
Tristan Salles <tristan.salles@sydney.edu.au>
2020-03-23 22:16:35 +1100

    Create install_HPC.sh

---
+++ install_HPC.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+module load gcc/4.9.3  python/3.6.5 petsc-gcc-mpich/3.11.1
+
+export F77FLAGS=-fPIC
+export FCFLAGS=-fPIC
+
+pip3 install -U fastfunc
+pip3 install -U meshplex
+
+python3 setup.py install --user
