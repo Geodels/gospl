0ba00bdb433b5e640b150cbe50c870ebbecdf09e
Tristan Salles <tristan.salles@sydney.edu.au>
2020-03-30 12:08:11 +1100

    add additional tests

---
+++ tests/test_gospl.py
@@ -0,0 +1,61 @@
+import pytest
+import warnings
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+
+def test_gospl_normal_run():
+    import gospl
+    from gospl.model import Model
+    import warnings
+
+    warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+    input = "/live/lib/gospl/tests/input/normal.yml"
+
+    # Initialise model
+    normal = Model(input, True, False)
+
+    # Run model
+    normal.runProcesses()
+
+    # Destroy model
+    normal.destroy()
+
+
+def test_gospl_backward_run():
+    import gospl
+    from gospl.model import Model
+    import warnings
+
+    warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+    input = "/live/lib/gospl/tests/input/backward.yml"
+
+    # Initialise model
+    normal = Model(input, True, False)
+
+    # Run model
+    normal.runProcesses()
+
+    # Destroy model
+    normal.destroy()
+
+
+def test_gospl_forward_run():
+    import gospl
+    from gospl.model import Model
+    import warnings
+
+    warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+    input = "/live/lib/gospl/tests/input/forward.yml"
+
+    # Initialise model
+    normal = Model(input, True, False)
+
+    # Run model
+    normal.runProcesses()
+
+    # Destroy model
+    normal.destroy()
