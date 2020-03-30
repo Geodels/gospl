e4a4edce8d3d2c9c3fc492434ab71ec1c8f662c0
Tristan Salles <tristan.salles@sydney.edu.au>
2020-03-30 11:42:25 +1100

    update test

--- tests/test_normal.py
+++ tests/test_normal.py
@@ -4,20 +4,7 @@ import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
-def test_gospl_init_destroy():
-    import gospl
-    from gospl.model import Model
-
-    input = "/live/lib/gospl/tests/input/normal.yml"
-
-    # Initialise model
-    normal = Model(input, True, False)
-
-    # Destroy model
-    normal.destroy()
-
-
-def test_gospl_run():
+def test_gospl_normal_run():
     import gospl
     from gospl.model import Model
 
