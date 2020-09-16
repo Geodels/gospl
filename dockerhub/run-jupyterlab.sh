8efc720ff49a7b561bae0e28601e68a764342fd1
Tristan Salles <tristan.salles@sydney.edu.au>
2020-09-16 11:30:16 +1000

    update

--- dockerhub/run-jupyterlab.sh
+++ dockerhub/run-jupyterlab.sh
@@ -3,15 +3,6 @@ cd /live
 export PATH=$PATH:/opt/mpich/bin/:/opt/petsc/bin
 export SHELL=/bin/bash
 
-set -x
-export DISPLAY=:99.0
-export PYVISTA_OFF_SCREEN=true
-export PYVISTA_USE_PANEL=true
-which Xvfb
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-sleep 3
-set +x
-
 jupyter lab --port=8888 --ip='*' --no-browser --allow-root --NotebookApp.token=''
 
 while true; do
