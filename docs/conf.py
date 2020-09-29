3aa4d408b522b4d98a9c229aadfafec0bdfd561f
Tristan Salles <tristan.salles@sydney.edu.au>
2020-09-29 16:47:04 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -88,9 +88,9 @@ language = None
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-# pygments_style = None
+pygments_style = None
 # pygments_style = "monokai"
-pygments_style = "default"
+# pygments_style = "default"
 sphinxemoji_style = "twemoji"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
