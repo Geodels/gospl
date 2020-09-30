e4df439ad72c670bfd4c1e70a14ccc9eb16f6a80
Tristan Salles <tristan.salles@sydney.edu.au>
2020-09-30 15:08:05 +1000

    put favicon in static folder

--- docs/conf.py
+++ docs/conf.py
@@ -131,7 +131,7 @@ html_css_files = [
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "images/favicon.ico"
+html_favicon = "favicon.ico"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
