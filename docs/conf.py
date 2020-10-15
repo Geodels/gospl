f0de3ecb3b19e99e634b9003b6e486b1eec42073
Tristan Salles <tristan.salles@sydney.edu.au>
2020-10-16 09:36:48 +1100

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -44,9 +44,9 @@ author = "Tristan Salles"
 # release = version
 
 # The short X.Y version
-version = "0.1.8"
+version = "0.2.0"
 # The full version, including alpha/beta/rc tags
-release = "0.1.8"
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------
