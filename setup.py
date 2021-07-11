6b0273c9556b8d13d9513c81de8946510b78a37b
Tristan Salles <tristan.salles@sydney.edu.au>
2021-07-12 09:46:09 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -130,7 +130,6 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
         ],
         cmdclass={"sdist": sdist},
     )
