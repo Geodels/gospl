cbd742b4d26568695bf6fd88a5eca69c6299bfc1
Tristan Salles <tristan.salles@sydney.edu.au>
2021-07-17 12:15:13 +1000

    update pypi version name

--- docs/conf.py
+++ docs/conf.py
@@ -50,9 +50,9 @@ author = "Tristan Salles"
 # release = version
 
 # The short X.Y version
-version = "0.2.8"
+version = "0.2.9"
 # The full version, including alpha/beta/rc tags
-release = "0.2.8"
+release = "0.2.9"
 
 # -- General configuration ---------------------------------------------------
 
