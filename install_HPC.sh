42f1ce56ecf925d1484966f8f7cc1d3dbe5dc3d5
Tristan Salles <tristan.salles@sydney.edu.au>
2021-07-24 09:59:37 +1000

    add installation to Artemis HPC

--- install_HPC.sh
+++ install_HPC.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-module load gcc/4.9.3  python/3.6.5 petsc-gcc-mpich/3.11.1  vtk/8.2.0-qt5.8.0
+module load gcc/4.9.3  python/3.6.5 petsc-gcc-mpich/3.11.1
 
 export F90=gfortran
 export F77FLAGS=-fPIC
 export FCFLAGS=-fPIC
 
-pip3 install vtk --user
+pip install Cython==0.29.22 --user
+pip install numpy==1.19.5 --user
+pip install meshplex==0.11.6 --user
+pip install meshio==1.0.3 --user
+pip install vtk==9.0.1 --user
+pip install h5py==2.9.0 --user
+pip install numpy-indexed==0.3.5 --user
+pip install pandas==1.0.3 --user
+pip install scipy==1.5.4 --user
+pip install ruamel.yaml==0.16.5 --user
+pip install mpi4py==3.0.0 --user
+pip install petsc4py==3.11.0 --user
+pip install scikit-fuzzy==0.4.2 --user
+
+mv requirements_hpc.txt requirements.txt
 
 python3 setup.py install --user
