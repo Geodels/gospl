5b489651805af1eeb5e4fb1c04591bb51753c700
Tristan Salles <tristan.salles@sydney.edu.au>
2021-07-27 11:49:50 +1000

    Update install_HPC.sh

--- install_HPC.sh
+++ install_HPC.sh
@@ -8,7 +8,7 @@ export FCFLAGS=-fPIC
 pip install Cython==0.29.22 --user
 pip install numpy==1.19.5 --user
 pip install meshplex==0.11.6 --user
-pip install meshio==1.0.3 --user
+pip install meshio==3.3.1 --user
 pip install vtk==9.0.1 --user
 pip install h5py==2.9.0 --user
 pip install numpy-indexed==0.3.5 --user
