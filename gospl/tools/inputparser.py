d514e32a0164a8264b986e05b67b3f2534fd8048
Utpal Singh <63436627+utpal-singh@users.noreply.github.com>
2022-08-14 18:13:45 +0530

    Update inputparser.py --> updated import statement for ruamel.yaml

--- gospl/tools/inputparser.py
+++ gospl/tools/inputparser.py
@@ -3,7 +3,7 @@ import sys
 import petsc4py
 import numpy as np
 import pandas as pd
-import ruamel.yaml as YAML
+from ruamel.yaml import YAML
 
 from operator import itemgetter
 from scipy.interpolate import interp1d
@@ -41,7 +41,7 @@ class ReadYaml(object):
 
         # Open YAML file
         with open(filename, "r") as finput:
-            yaml = YAML()
+            typ="unsafe", pure=True
             self.input = yaml.load(finput)
 
         if MPIrank == 0 and "name" in self.input.keys() and self.verbose:
